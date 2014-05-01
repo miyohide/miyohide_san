f352632c10431ba6bca2133add0e5dd0e7280333
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-05-01 20:57:47 +0900

    Add clock setting

--- lib/miyohide_san.rb
+++ lib/miyohide_san.rb
@@ -1,5 +1,17 @@
+require 'settingslogic'
+require "action_mailer"
+require "clockwork"
 require "miyohide_san/version"
+require "miyohide_san/settings"
+require "miyohide_san/doorkeeper"
+require "miyohide_san/event"
+require "miyohide_san/postman"
+require File.expand_path('../../config/application', __FILE__)
 
-module MiyohideSan
-  # Your code goes here...
+include Clockwork
+
+every(1.day, 'notify.job', at: '05:00') do
+  if event = MiyohideSan::Event.find_by_after_week
+    MiyohideSan::Postman.notify(event).deliver
+  end
 end
