0613a60148b55ed4852a21fb16c02ed03dba92bb
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-05-01 21:50:08 +0900

    Add yaffle

--- lib/miyohide_san.rb
+++ lib/miyohide_san.rb
@@ -1,11 +1,13 @@
 require 'settingslogic'
 require "action_mailer"
 require "clockwork"
+require "twitter"
 require "miyohide_san/version"
 require "miyohide_san/settings"
 require "miyohide_san/doorkeeper"
 require "miyohide_san/event"
 require "miyohide_san/postman"
+require "miyohide_san/yaffle"
 require File.expand_path('../../config/application', __FILE__)
 
 include Clockwork
