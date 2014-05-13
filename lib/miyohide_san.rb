e4d32aacbdc7bb66394ef8d15f12f18a6f612624
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-05-13 20:05:27 +0900

    Add rake task

--- lib/miyohide_san.rb
+++ lib/miyohide_san.rb
@@ -2,7 +2,6 @@ require "dotenv"
 Dotenv.load
 
 require "action_mailer"
-require "clockwork"
 require "twitter"
 require 'settingslogic'
 require "miyohide_san/version"
@@ -13,11 +12,14 @@ require "miyohide_san/postman"
 require "miyohide_san/yaffle"
 require File.expand_path('../../config/application', __FILE__)
 
-include Clockwork
 
-every(1.day, 'notify.job', at: '20:00') do
-  if event = MiyohideSan::Event.find_by_after_week
-    MiyohideSan::Postman.notify(event).deliver
-    MiyohideSan::Yaffle.new(event).tweet
+module MiyohideSan
+  def notify
+    if event = MiyohideSan::Event.find_by_after_week
+      MiyohideSan::Postman.notify(event).deliver
+      MiyohideSan::Yaffle.new(event).tweet
+    end
   end
+
+  module_function :notify
 end
