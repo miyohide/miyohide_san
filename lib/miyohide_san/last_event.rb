1071aef0db8544acdb61baea527be120b23e992a
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-05-14 23:23:10 +0900

    Add cached event

---
+++ lib/miyohide_san/last_event.rb
@@ -0,0 +1,23 @@
+class MiyohideSan::LastEvent
+  include Comparable
+  CACHE_DIR = File.expand_path('../../../tmp', __FILE__)
+
+  def <=>(new_event)
+    result = nil
+
+    cache.fetch('event') { new_event }.tap do |event|
+      result = (event.id <=> new_event.id)
+    end
+
+    if result.try(:<, 0)
+      cache.write('event', new_event)
+    end
+
+    result
+  end
+
+  private
+  def cache
+    @cache ||= ActiveSupport::Cache::FileStore.new(CACHE_DIR)
+  end
+end
