857d148aeddb4a7c9236ee6a4cdeeb58cee583cc
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-05-16 20:23:19 +0900

    Add newborn method

--- lib/miyohide_san/postman.rb
+++ lib/miyohide_san/postman.rb
@@ -2,9 +2,14 @@ module MiyohideSan
   class Postman < ActionMailer::Base
     default from: Settings.mail.from, return_path: Settings.mail.to
 
-    def notify(event)
+    def testament(event)
       @event = event
       mail to: Settings.mail.to, subject: "#{@event.title} 開催のお知らせ"
+    end
+
+    def newborn(event)
+      @event = event
+      mail to: Settings.mail.to, subject: "#{@event.title} 募集開始のお知らせ"
     end
   end
 end
