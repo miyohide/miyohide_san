472c7505989a76ac277b1613f8ec5bd1b955a4c2
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-06-06 22:08:07 +0900

    Add group model

---
+++ lib/miyohide_san/group.rb
@@ -0,0 +1,15 @@
+module MiyohideSan
+  class Group
+    include Mongoid::Document
+
+    field :id, type: Integer
+    field :name, type: String
+    field :country_code, type: String
+    field :logo, type: String
+    field :description, type: String
+    field :public_url, type: String
+
+    embedded_in :event, inverse_of: :event
+
+  end
+end
