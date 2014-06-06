dff43650e7770a9aeb2e3b41f59e29a991930542
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-06-06 22:11:00 +0900

    Add rake tasks

--- lib/miyohide_san.rb
+++ lib/miyohide_san.rb
@@ -1,56 +1,39 @@
 require "dotenv"
 Dotenv.load
 
-require 'forwardable'
-require "twitter"
-require 'settingslogic'
-require "action_mailer"
-require 'active_support/dependencies/autoload'
-
-require "doorkeeper"
+require "uri"
+require "net/http"
+require "mongoid"
+require "settingslogic"
+require 'active_support/all'
 require "miyohide_san/version"
-require "miyohide_san/settings"
-
-require File.expand_path('../../config/application', __FILE__)
 
 module MiyohideSan
   extend ActiveSupport::Autoload
+
+  autoload :Settings
+  autoload :Zapierable
   autoload :Event
+  autoload :Group
   autoload :Postman
   autoload :Yaffle
-  autoload :LastEvent
-  autoload :Gmail
+  autoload :Doorkeeper
 
-  def logger
-    @logger ||= Logger.new(STDOUT)
-  end
+  def recent
+    if doorkeeper = Doorkeeper.recent
+      Event.new(doorkeeper).tap do |event|
+        Yaffle::PreviousNotice.new(event).tap do |yaffle|
+          yaffle.post
+        end
 
-  def testament
-    if event = Event.find_by_one_week_later
-      #Postman.testament(event).deliver
-      Yaffle::Testament.new(event).tweet
+        Postman::PreviousNotice.new(event).tap do |postman|
+          postman.post
+        end
+      end
     end
-
-    MiyohideSan.logger.info "testament: #{event}"
   end
 
-  def newborn
-    event = Event.last
-    if event.new_record?
-      #Postman.newborn(event).deliver
-      Yaffle::Newborn.new(event).tweet
-    end
-
-    MiyohideSan.logger.info "newborn: #{event}"
-  end
-
-  def login
-    Gmail.login
-  end
-
-  def test
-    Postman.test.deliver
-  end
-
-  module_function :testament, :newborn, :login, :test
+  module_function :recent
 end
+
+require File.expand_path('../../config/application', __FILE__)
