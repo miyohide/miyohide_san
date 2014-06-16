6d2eacafe1c77ccf24b1d41db5bc0cbe11967c0e
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-06-16 19:58:17 +0900

    Add spec for postman#subject

--- spec/lib/miyohide_san/postman_spec.rb
+++ spec/lib/miyohide_san/postman_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe MiyohideSan::Postman::Announcement do
   let(:event) { create(:miyohide_san_event) }
   let(:announcement) { MiyohideSan::Postman::Announcement.new(event) }
-  # it { expect(announcement.body).to match /#{event.formatted_starts_at}|#{event.weekday}|#{event.title}/ }
 
   describe "#body" do
     subject { announcement.body }
@@ -50,5 +49,14 @@ end
 describe MiyohideSan::Postman::PreviousNotice do
   let(:event) { create(:miyohide_san_event) }
   let(:notice) { MiyohideSan::Postman::PreviousNotice.new(event) }
-  it { expect(notice.body).to match /#{event.formatted_starts_at}|#{event.weekday}|#{event.title}/ }
+
+  describe "#body" do
+    subject { notice.body }
+    it { is_expected.to match /#{event.formatted_starts_at}|#{event.weekday}|#{event.title}/ }
+  end
+
+  describe "#subject" do
+    subject { notice.subject }
+    it { is_expected.to match /#{event.title}/ }
+  end
 end
