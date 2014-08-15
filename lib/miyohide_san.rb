11ec0cd4dafeaa0886c18224b26506c5a0e687cd
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-08-15 14:17:27 +0900

    rakeをrequireするファイルを分割した

--- lib/miyohide_san.rb
+++ lib/miyohide_san.rb
@@ -2,7 +2,6 @@ require "uri"
 require "date"
 require "net/http"
 require "ostruct"
-require "rake"
 require "mongoid"
 require "settingslogic"
 require 'active_support/all'
@@ -26,8 +25,3 @@ end
 # Mongoid Setting
 #
 Mongoid.load!(File.expand_path('../../config/mongoid.yml', __FILE__), (ENV["RACK_ENV"] || "development"))
-
-#
-# Rake Setting
-#
-Rake.application.rake_require('miyohide_san', [File.expand_path('../tasks', __FILE__)])
